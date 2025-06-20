74a110dead841f61ce8e773996dcfcb18b05c223
josevitor555 <koservitor123@gmail.com>
2025-06-20 20:16:40 -0300

    Matematicando - 20/06/2026

---
+++ mathgame/src/App.tsx
@@ -0,0 +1,13 @@
+
+const App = () => {
+
+  return (
+    <>
+      <div className='app'>
+        <h1 className="text-3xl font-bold underline"> Olá, mundo. </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
